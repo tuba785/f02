8fc89a138728f124732a10193909790fa174341c
tuba785 <turalbabayev785@gmail.com>
2025-11-29 22:44:32 +0400

    js final added

--- Tasks/task15_29-11/proj/src/App.jsx
+++ Tasks/task15_29-11/proj/src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import Home from './pages/Home/home';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home/home";
 
 function App() {
   return (
